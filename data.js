fcfd3fd6979dfd6e068c61d723594fefc0c55a0e
E13VEN011 <151870030+E13VEN011@users.noreply.github.com>
2024-08-23 11:25:33 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,78 @@
+var product = [{
+    "id": 0,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+}, {
+    "id": 1,
+    "title": "Samsung Galaxy S22",
+    "price": 179999,
+    "description": "8 GB RAM | 128 GB ROM15.49 cm (6.1 inch) Full HD+ Display50MP + 12MP + 10MP | 10MP Front Camera3700 mAh Lithium-ion Battery Snapdragon 8 Gen 1 Processor",
+    "brand": "Samsung",
+    "image": "https://www.whatmobile.com.pk/admin/images/Samsung/SamsungGalaxyS22-b.jpg"
+}, {
+    "id": 2,
+    "title": "Vivo X80",
+    "price": 159999,
+    "description": "8 GB RAM | 128 GB ROM17.22 cm (6.78 inch) Full HD+ Display50MP + 12MP + 12MP | 32MP Front Camera4500 mAh Lithium BatteryMediatek Dimensity 9000 Processor",
+    "brand": "Vivo",
+    "image": "https://www.whatmobile.com.pk/admin/images/Vivo/VivoX80-b.jpg"
+}, {
+    "id": 3,
+    "title": "Oppo F21 Pro 5G",
+    "price":  69999,
+    "description": "8 GB RAM | 128 GB ROM16.33 cm (6.43 inch) Display16MP Rear Camera4500 mAh Battery",
+    "brand": "Oppo",
+    "image": "https://www.whatmobile.com.pk/admin/images/Oppo/OppoF21Pro5G-b.jpg"
+}, {
+    "id": 4,
+    "title": "Infinix Note 12",
+    "price": 36999,
+    "description": "6 GB RAM | 128 GB ROM | Expandable Upto 512 GB17.02 cm (6.7 inch) Full HD+ AMOLED Display50MP + 2MP Depth + AI Lens | 16MP Front Camera5000 mAh Li-ion Polymer Battery MediaTek Helio G88 Processor",
+    "brand": "Infinix",
+    "image": "https://www.whatmobile.com.pk/admin/images/Infinix/InfinixNote12-b.jpg"
+}, {
+    "id": 5,
+    "title": "Samsung Galaxy A13",
+    "price": 35499,
+    "description": "4 GB RAM | 64 GB ROM | Expandable Upto 1 TB 16.76 cm (6.6 inch) Full HD+ Display 50MP + 5MP + 2MP + 2MP | 8MP Front Camera 5000 mAh Lithium Ion Battery Exynos Octa Core Processor",
+    "brand": "Samsung",
+    "image": "https://www.whatmobile.com.pk/admin/images/Samsung/SamsungGalaxyA13-b.jpg"
+}, {
+    "id": 6,
+    "title": "Xiaomi 12",
+    "price": 179999,
+    "description": "6 GB RAM | 128 GB ROM | Expandable Upto 1 TB 17.25 cm (6.79 inch) Full HD+ Display 50MP + 2MP | 8MP Front Camera 5000 mAh Battery Snapdragon 4 Gen 2 Processor",
+    "brand": "Xiaomi",
+    "image": "https://www.whatmobile.com.pk/admin/images/Xiaomi/Xiaomi12-b.jpg"
+}, {
+    "id": 7,
+    "title": "Apple iPhone 15 ",
+    "price": 107999,
+    "description": "128 GB ROM 15.49 cm (6.1 inch) Super Retina XDR Display 48MP + 12MP | 12MP Front Camera A16 Bionic Chip, 6 Core Processor Processor",
+    "brand": "Apple",
+    "image": "https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/h/d/9/-original-imagtc2qzgnnuhxh.jpeg?q=70&crop=false"
+}, {
+    "id": 8,
+    "title": "Realme C35",
+    "price": 32999,
+    "description": "4 GB RAM | 128 GB ROM | Expandable Upto 1 TB 16.76 cm (6.6 inch) Full HD+ Display 50MP + 2MP + 0.3MP | 8MP Front Camera 5000 mAh Lithium Polymer Battery Unisoc Tiger T616 Processor",
+    "brand": "Realme",
+    "image": "https://www.whatmobile.com.pk/admin/images/Realme/RealmeC35-b.jpg"
+}, {
+    "id": 9,
+    "title": "Tecno Spark 8C 3GB",
+    "price": 20499,
+    "description": "8 GB RAM | 128 GB ROM | Expandable Upto 512 GB 16.76 cm (6.6 inch) HD Display 50MP Rear Camera | 8MP Front Camera 5000 mAh Battery Helio G36 Processor",
+    "brand": "Tecno",
+    "image": "https://www.whatmobile.com.pk/admin/images/Tecno/TecnoSpark8C3GB-b.jpg"
+}, {
+    "id": 10,
+    "title": "Motorola G85 5G",
+    "price": 16299,
+    "description": "8 GB RAM | 128 GB ROM 16.94 cm (6.67 inch) Full HD+ Display 50MP + 8MP | 32MP Front Camera 5000 mAh Battery6s Gen 3 Processor",
+    "brand": "Motorola",
+    "image": "https://motorolain.vtexassets.com/arquivos/ids/159081-1200-auto?width=1200&height=auto&aspect=true"
+}]
